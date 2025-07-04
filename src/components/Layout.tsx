6b46d056a583ad02732434c36376c9260c710be4
raulbaciulescu <78053229+raulbaciulescu@users.noreply.github.com>
2025-07-04 19:31:38 +0300

    Fix navbar to show profile image only on the left

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -99,18 +99,6 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
                           </button>
                         </div>
                       </div>
-                      <div className="w-8 h-8 bg-yellow-500 rounded-full flex items-center justify-center overflow-hidden">
-                        {user?.profileImage ? (
-                          <img
-                            src={user.profileImage}
-                            alt={user.name}
-                            className="w-full h-full object-cover"
-                          />
-                        ) : (
-                          <User className="h-4 w-4 text-white" />
-                        )}
-                      </div>
-                    </div>
                 ) : (
                     <div className="flex items-center space-x-2">
                       <Link
